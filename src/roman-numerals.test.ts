e1411c09c3a26ee0483d2520cc63110e3e89fa3a
e83551u <caner.gul9@etu.univ-lorraine.fr>
2025-05-23 11:08:58 +0200

    Ajout des premiers test et implémentation de la class

--- src/roman-numerals.test.ts
+++ src/roman-numerals.test.ts
@@ -1,1 +1,21 @@
-// Write your test here
+import { it, expect ,test } from "vitest";
+
+test.each([
+  [ "I",1 ],
+])
+class RomanNumeral {
+  of(valeurRom: string): number {
+    if (valeurRom === "I") return 1;
+    if (valeurRom === "IV") return 4;
+    return 0;
+  }
+}
+
+it("Devrait convertir I en 1", function () {
+  const romain = new RomanNumeral();
+  expect(romain.of("I")).toBe(1);
+});
+it("Devrait convertir", function () {
+  const romain = new RomanNumeral();
+  expect( romain.of("IV") ).toBe(4);
+})
